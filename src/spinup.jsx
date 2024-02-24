2b966ed886ade07c08fb7e59961ed6492eb98513
KrusherK77 <Jk4thefox@gmail.com>
2024-02-24 14:17:43 -0500

    commit

---
+++ src/spinup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class spinup extends React.Component {
+  render() {
+    return (
+      <div className="content">
+        <h1>Spinning...</h1>
+      </div>
+    );
+  }
+}
+
+export default HowToPlayPage;
