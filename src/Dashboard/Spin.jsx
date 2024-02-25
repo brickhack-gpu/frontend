d2e675ea62624d0305707602195e7c6d0d69f3a9
KrusherK77 <Jk4thefox@gmail.com>
2024-02-25 07:40:15 -0500

    730am

--- src/Dashboard/Spin.jsx
+++ src/Dashboard/Spin.jsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Sidebar from './Sidebar';
 import { Link } from "react-router-dom";
+import { AuthGet } from "../API";
 
 function Spin() {
+    const [available, setAvailable] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+      (async() => {
+        try {
+          setAvailable((await AuthGet('/search')).server_configs);
+        } catch(e) {
+          setError(e.toString());
+        }
+      })();
+    }, []);
     return (
-      <>
-        <Sidebar />
-
-        
+        <>
+          <Sidebar />
+          { error ? <p className="error">{error}</p> : null }
+          
         </>
-
       );
     }
 
