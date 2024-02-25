c9609f40c5210bce10a173cda68acea83da10aa2
git <kv@value.tt>
2024-02-25 07:15:55 -0500

    more shit

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,45 +1,84 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./Dashboard/Sidebar";
 import { Link } from "react-router-dom";
+import { AuthGet } from "./API";
 
 function dashboard() {
-    const [availableServers, setAvailableServers] = useState([{
-        type: 'GTX-3600',
-        count: 5,
-        region: 'United States',
-        status: "Active",
-        price: 0.22,
-        id: 1,
-    }, {
-        type: 'GTX-4090',
-        count: 3,
-        region: 'Canada',
-        status: "Inactive",
-        price: 0.65,
-        id: 2,
-    }])
+    const [servers, setServers] = useState([]);
+    const [error, setError] = useState('');
+    const [deposits, setDeposits] = useState([]);
+    const [purchases, setPurchases] = useState([]);
+    const [data, setData] = useState({
+        active: 0,
+        costs: 0,
+        disk: 0,
+    });
+    const [update, setUpdate] = useState(0);
+
+    useEffect(() => {
+        (async() => {
+            try {
+                const transactions = await AuthGet('/transactions');
+                setDeposits(transactions.deposits);
+                setPurchases(transactions.purchases);
+                setData(await AuthGet('/data'));
+                setServers((await AuthGet('/products')).products);
+            } catch(e) {
+                setError(e.toString());
+            }
+        })();
+    }, [update]);
 
     return (
         <div className="dash">
             <Sidebar />
             <div className="dashcontent">
+                { error ? <p className="error">{error}</p> : null }
                 <div className="dashmain">
                 <div className="stats">
-                    <h1>Active VMs: 0/3</h1>
-                    <p>Hourly Rate: $0.47</p>
-                    <p>Storage Used: 1336B</p>
+                    <h1>{`Active VMs: ${data.active}/3`}</h1>
+                    <p>{`Hourly Rate: $${data.costs}`}</p>
+                    <p>{`Storage Used: ${data.disk} GB`}</p>
                 </div>
-                <div className="transactions">
+                <div className="purchases">
                     <table>
                         <thead>
                             <tr>
                                 <th>Amount</th>
-                                <th>Type</th>
                                 <th>Date</th>
                             </tr>
                         </thead>
-                        <tbody></tbody>
+                        <tbody>
+                            {purchases.map(purchase => {
+                                return (
+                                    <tr key={purchase.id}>
+                                        <th>{`$${purchase.amount}`}</th>
+                                        <th>{purchase.createdAt}</th>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="deposits">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Amount</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {deposits.map(deposit => {
+                                return (
+                                    <tr key={deposit.id}>
+                                        <th>{`$${deposit.amount}`}</th>
+                                        <th>{deposit.createdAt}</th>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
                     </table>
                 </div>
                 <div className="gpu-container">
@@ -50,18 +89,18 @@ function dashboard() {
                                 <th>Price</th>
                                 <th>Region</th>
                                 <th>Status</th>
-                                <th>Max Available</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {availableServers.map(available => {
+                            {servers.map(server => {
                                 return (
-                                    <tr key={available.id} className="gpu">
-                                        <th>{available.type}</th>
-                                        <th>{`$${available.price}/h`}</th>
-                                        <th>{available.region}</th>
-                                        <th>{available.status}</th>
-                                        <th>{`${available.count}`}</th>
+                                    <tr key={server.id} className="gpu">
+                                        <th>{`${server.server_config.gpu_type} ${server.server_config.gpu_type}`}</th>
+                                        <th>{`$${server.price}/h`}</th>
+                                        <th>{server.server_config.region}</th>
+                                        <th>{server.status}</th>
+                                        <th><button onClick={() => { window.open(server.dns_link, '_blank') }}>Access</button></th>
                                     </tr>
                                 );
                             })}
