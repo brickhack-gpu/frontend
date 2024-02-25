6c28338151baca68a9103990d864aed0c6b0781d
git <kv@value.tt>
2024-02-25 06:29:42 -0500

    api shit

--- src/login.jsx
+++ src/login.jsx
@@ -2,45 +2,65 @@ import React from "react";
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
+import { PostLogin, PostRegister } from './API.js';
 
 function Login() {
-  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [working, setWorking] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Password:', password);
+  async function handleLogin() {
+    setWorking(true);
+    setError('');
 
-    // Clear the form (If you want to clear the form after submit)
-    setName('');
-    setEmail('');
-    setPassword('');
+    try {
+        await PostLogin(email, password);
+    } catch(e) {
+        setError(e.toString());
+    }
+
+    setWorking(false);
+  }
+
+  async function handleRegister() {
+    setWorking(true);
+    setError('');
+
+    try {
+        await PostRegister(email, password);
+    } catch(e) {
+        setError(e.toString());
+    }
+
+    setWorking(false);
   }
 
   return (
     <>
       <Navbar />
       <div className="login1-container">
-        <form onSubmit={handleSubmit} className="login1-form">
+        <form className="login1-form">
+        { error ? <p className="error">{error}</p> : null }
         <img src="public\botl.png" className="logo123" alt="Logo" />
-          <input 
+          <input
             className="login12-input"
             type="email"
             value={email}
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
           />
-          <input 
+          <input
             className="login1-input"
             type="password"
             value={password}
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button type="submit" className="login1-button">Login</button>
+          <div className="inbtn">
+              <button onClick={handleLogin} disabled={working} type="submit" className="login1-button">Login</button>
+              <button onClick={handleRegister} disabled={working} type="submit" className="login1-button">Register</button>
+          </div>
         </form>
       </div>
     </>
